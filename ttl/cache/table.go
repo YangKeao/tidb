84204fb3107469121e20d35d1b90867ccb501f12
YangKeao <yangkeao@chunibyo.icu>
2022-11-29 21:09:23 +0800

    add info schema cache and ttl status table cache
    Signed-off-by: YangKeao <yangkeao@chunibyo.icu>

--- ttl/cache/table.go
+++ ttl/cache/table.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//	http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -123,6 +123,10 @@ func NewPhysicalTable(schema model.CIStr, tbl *model.TableInfo, partition model.
 		KeyColumnTypes: keyColumTypes,
 		TimeColumn:     timeColumn,
 	}, nil
+}
+
+func isTTLTable(tbl *model.TableInfo) bool {
+	return tbl.TTLInfo != nil && tbl.State == model.StatePublic
 }
 
 // ValidateKey validates a key
