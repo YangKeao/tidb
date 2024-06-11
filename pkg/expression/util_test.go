910aca61a982b3e5ce9960ffd0333c55ce0bbc80
Yang Keao <yangkeao@chunibyo.icu>
2024-06-11 14:44:02 +0800

    add ParamValues interface
    Signed-off-by: Yang Keao <yangkeao@chunibyo.icu>

--- pkg/expression/util_test.go
+++ pkg/expression/util_test.go
@@ -558,7 +558,7 @@ func (m *MockExpr) VecEvalJSON(ctx EvalContext, input *chunk.Chunk, result *chun
 	return nil
 }
 
-func (m *MockExpr) StringWithCtx(EvalContext) string { return "" }
+func (m *MockExpr) StringWithCtx(ParamValues) string { return "" }
 func (m *MockExpr) MarshalJSON() ([]byte, error)     { return nil, nil }
 func (m *MockExpr) Eval(ctx EvalContext, row chunk.Row) (types.Datum, error) {
 	return types.NewDatum(m.i), m.err
